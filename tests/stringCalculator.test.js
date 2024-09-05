d92f61fd1d18fee043613ac125ef9e4511ce83dd
Narendra <narendraparora55@gmail.com>
2024-09-05 14:01:06 +0530

    Handle negative numbers with error

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -5,6 +5,10 @@
 // });
 
 
-test('supports different delimiters', () => {
-    expect(add("//;\n1;2")).toBe(3);
+// test('supports different delimiters', () => {
+//     expect(add("//;\n1;2")).toBe(3);
+// });
+
+test('throws an error for negative numbers', () => {
+    expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
 });
