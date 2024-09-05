4cf61748b9d83b2ae52b7bb79feaf4a2d6dcf686
Narendra <narendraparora55@gmail.com>
2024-09-05 13:57:57 +0530

    Handle single and multiple numbers separated by commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,16 @@
 // stringCalculator.js
 
-function add(numbers) {
-    return 0;
-}
+// function add(numbers) {
+//     return 0;
+// }
 
-module.exports = { add };
+// module.exports = { add };
+
+test('returns the number when there is one number', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('returns the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
+
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,5 +1,13 @@
-const { add } = require('../src/stringCalculator');
+// const { add } = require('../src/stringCalculator');
 
-test('returns 0 for an empty string', () => {
-    expect(add("")).toBe(0);
-});
+// test('returns 0 for an empty string', () => {
+//     expect(add("")).toBe(0);
+// });
+
+function add(numbers) {
+    if (numbers === "") return 0;
+
+    const numberArray = numbers.split(',');
+    const sum = numberArray.reduce((acc, curr) => acc + parseInt(curr), 0);
+    return sum;
+}
